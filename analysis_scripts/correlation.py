4c37379ce4e6383498c352298f25bfd00f99eb1a
viktoria.tisler <viktoria.tisler@gmail.com>
2020-01-01 14:54:13 +0200

    EDA notebook

---
+++ analysis_scripts/correlation.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+def plot_correlation(data, method):
+    fig, ax = plt.subplots(figsize=(60, 60))
+    sns.set(font_scale=10)
+    correlation_matrix = data.corr(method=method, min_periods=1)
+    sns.heatmap(correlation_matrix, ax=ax, cmap="plasma", fmt="d")
+    ax.set_ylabel('')
+    ax.set_xlabel('')
+    plt.show()
+
+
+def drop_lin_correlated(df_in, method):
+    lower_bound = -0.8
+    upper_bound = 0.8
+    corr_matrix = df_in.corr(method=method, min_periods=1).abs()
+    # the matrix is symmetric so we need to extract upper triangle matrix without diagonal (k = 1)
+    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
+
+    # Find indices of feature columns with correlation greater than 0.80
+    features_above_max = [column for column in upper.columns if any(upper[column] > upper_bound)]
+    # Find indices of feature columns with correlation less than -0.8
+    features_below_min = [column for column in upper.columns if any(upper[column] < lower_bound)]
+    features_to_drop = features_above_max + features_below_min
+    df_out = df_in.drop(features_to_drop, axis=1)
+
+    return df_out
+
+def plot_correlation_bivariate(df, fre1, fre2):
+    ax = plt.subplot(111, xlabel=fre1, ylabel=fre2, title='Features correlation')
+    for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                  ax.get_xticklabels() + ax.get_yticklabels()):
+        item.set_fontsize(15)
+    plt.scatter(df[fre1], df[fre2])
+    plt.show()
